8d3cced579f04b2586cc028c5ac0e684a501f9cb
Ekbal41 <mdasifekbalshagor@gmail.com>
2022-11-21 12:52:10 +0600

    first commit

---
+++ app.py
@@ -0,0 +1,41 @@
+from flask import Flask, render_template, request
+from chatterbot import ChatBot
+from chatterbot.trainers import ChatterBotCorpusTrainer
+
+
+app = Flask(__name__)
+import spacy
+nlp=spacy.load('en')
+
+asfiya = ChatBot(
+    'train',
+    storage_adapter='chatterbot.storage.SQLStorageAdapter',
+    logic_adapters=[
+        {
+            'import_path': 'chatterbot.logic.BestMatch',
+            'default_response': 'I am sorry, but I do not understand.',
+            'maximum_similarity_threshold': 0.90
+        }
+    ],
+    database_uri='sqlite:///database-chatbot.db'
+)
+
+trainer = ChatterBotCorpusTrainer(asfiya)
+trainer.train("chatterbot.corpus.english")
+trainer.train("chatterbot.corpus.english.greetings")
+trainer.train("chatterbot.corpus.english.conversations")
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/get")
+def get_bot_response():
+    userText = request.args.get('msg')
+    return str(asfiya.get_response(userText))
+
+
+if __name__ == "__main__":
+    app.run()
